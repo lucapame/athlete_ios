367031101f383cac7faae64058f2e1f1667c31dc
Luis Parra <LUIS_PARRA@homedepot.com>
2024-04-23 17:59:31 -0600

    Initial Commit

---
+++ athlete.fitness/athlete_fitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  athlete_fitnessApp.swift
+//  athlete.fitness
+//
+//  Created by LUIS PARRA on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct athlete_fitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
